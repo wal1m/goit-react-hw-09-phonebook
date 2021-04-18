3bd1287362aa6097eb939e05fbb50c37e1bef66d
wal1m <wal1m@yandex.ru>
2021-04-18 19:24:38 +0300

    Updates

---
+++ static/js/HomeView.a390be1f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-09-phonebook"]=this["webpackJsonpgoit-react-hw-09-phonebook"]||[]).push([[1],{173:function(o,t,e){"use strict";e.r(t);var n=e(2);t.default=function(){return Object(n.jsx)("h1",{children:"\u0414\u043e\u0431\u0440\u043e \u043f\u043e\u0436\u0430\u043b\u043e\u0432\u0430\u0442\u044c \u0432 Phonebook"})}}}]);
+//# sourceMappingURL=HomeView.a390be1f.chunk.js.map
